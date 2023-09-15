be105f9be2bd66f145e71be1dde4b7820f20b871
Abdul Rahman <131333352+Abdul-Rahm@users.noreply.github.com>
2023-09-15 17:59:31 +0530

    Delete src directory

--- src/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    rules: {
-        "react-hooks/exhaustive-deps": "off",
-        "new-cap": "off",
-        "jsx-a11y/alt-text": "off",
-        "prefer-const": "off",
-    }
-
-}
